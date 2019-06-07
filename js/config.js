eabe21eb2c31b4ec82e8e1665d00eadc09154ff2
Franky2025 <30404801+Franky2025@users.noreply.github.com>
2019-06-07 09:00:24 +0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -92,7 +92,7 @@ var l_musicRandom = false;
 
 /*Music volume
 Choose a value between 0 and 100*/
-var l_musicVolume = 5;
+var l_musicVolume = 50;
 
 /*Enable custom messages?*/
 var l_messagesEnabled = true;
